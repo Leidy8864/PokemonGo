bc119f8028c15fd71c798ad1708765dd0e5a6edb
Leidy C_S <leidy.callupe@tecsup.edu.pe>
2019-05-24 22:29:56 -0500

    first commit

---
+++ PokemonGo/Funciones.swift
@@ -0,0 +1,52 @@
+import UIKit
+import CoreData
+
+func agregarPokemons(){
+    crearPokemon(xnombre : "Mew", ximagenNombre: "mew")
+    crearPokemon(xnombre: "Meowth", ximagenNombre: "meowth")
+    crearPokemon(xnombre: "Abra", ximagenNombre: "abra")
+    crearPokemon(xnombre: "Bullbasaur", ximagenNombre: "bullbasaur")
+    crearPokemon(xnombre: "Dratini", ximagenNombre: "dratini")
+    crearPokemon(xnombre: "Eevee", ximagenNombre: "eevee")
+    crearPokemon(xnombre: "Mankey", ximagenNombre: "mankey")
+    (UIApplication.shared.delegate as! AppDelegate).saveContext()
+}
+func crearPokemon(xnombre:String, ximagenNombre:String){
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let pokemon = Pokemon(context:context)
+    pokemon.nombre = xnombre
+    pokemon.imagenNombre = ximagenNombre
+}
+
+func obtenerPokemons() -> [Pokemon]{
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    do{
+        let pokemons = try context.fetch(Pokemon.fetchRequest()) as! [Pokemon]
+        if pokemons.count == 0 {
+            agregarPokemons()
+            return obtenerPokemons()
+        }
+        return pokemons
+    }catch{}
+    return []
+}
+func obtenerPokemonsAtrapados() -> [Pokemon]{
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let queryConWhere = Pokemon.fetchRequest() as NSFetchRequest<Pokemon>
+    queryConWhere.predicate = NSPredicate(format: "atrapado == %@", true as CVarArg)
+    do{
+        let pokemons = try context.fetch(queryConWhere) as [Pokemon]
+        return pokemons
+    }catch{}
+    return []
+}
+func obtenerPokemonsNoAtrapados() -> [Pokemon]{
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let queryConWhere = Pokemon.fetchRequest() as NSFetchRequest<Pokemon>
+    queryConWhere.predicate = NSPredicate(format: "atrapado == %@", false as CVarArg)
+    do{
+        let pokemons = try context.fetch(queryConWhere) as [Pokemon]
+        return pokemons
+    }catch{}
+    return []
+}
---
+++ PokemonGo/PokedexViewController.swift
@@ -0,0 +1,62 @@
+import UIKit
+
+class PokedexViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    var pokemonsAtrapados:[Pokemon] = []
+    var pokemonsNoAtrapados:[Pokemon] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        pokemonsAtrapados = obtenerPokemonsAtrapados()
+        pokemonsNoAtrapados = obtenerPokemonsNoAtrapados()
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let pokemon = pokemonsAtrapados[indexPath.row]
+            pokemonsAtrapados.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            pokemon.atrapado = false
+            pokemonsAtrapados = obtenerPokemonsAtrapados()
+            pokemonsNoAtrapados = obtenerPokemonsNoAtrapados()
+            tableView.reloadData()
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0{
+            return "Atrapados"
+        } else {
+            return "No Atrapados"
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return pokemonsAtrapados.count
+        } else {
+            return pokemonsNoAtrapados.count
+        }
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let pokemon : Pokemon
+        if indexPath.section == 0 {
+            pokemon = pokemonsAtrapados[indexPath.row]
+        }else{
+            pokemon = pokemonsNoAtrapados[indexPath.row]
+        }
+        let cell = UITableViewCell()
+        cell.textLabel?.text = pokemon.nombre
+        cell.imageView?.image = UIImage(named: pokemon.imagenNombre!)
+        return cell
+    }
+    @IBAction func mapTapped(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
